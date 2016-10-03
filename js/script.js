d3c3580c797c9ac0c6c29a180255a80a3c5bb402
mneumayer <matthew.neumayer@gmail.com>
2016-10-02 22:08:44 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,8 +14,19 @@ function loadData() {
     // load streetview
 
     // YOUR CODE GOES HERE!
+    var street = $('#street').val();
+    var city = $('#city').val();
+    var address = street + ", " + city;
+
+    var googleImgReq = "https://maps.googleapis.com/maps/api/streetview?+size=600x400&location=" + address;
+
+     $greeting.text("So, you want to live at " + address + "?")
+
+    $body.append('<img class="bgimg" src = googleImgReq >');
+
+
 
     return false;
 };
-
+// GOOGLE API KEY = AIzaSyB5HHpPzai3_eTyjEoBcdbhgtbLnMXz6VY
 $('#form-container').submit(loadData);
